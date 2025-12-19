924fb5bf9c68c12d3cb8256ee11716b7b7848585
rizky2pratomo <118192430+rizky2pratomo@users.noreply.github.com>
2025-12-19 16:12:47 +0000

    finish finish project

--- js/script.js
+++ js/script.js
@@ -24,10 +24,10 @@ function validateForm() { }
 <script>
   const links = document.querySelectorAll(".nav-link");
 
-  links.forEach(link => {
+  links.forEach(link = {
     link.addEventListener("click", function() {
       links.forEach(l => l.classList.remove("active"));
       this.classList.add("active");
-    });
+    })
   });
 </script>
